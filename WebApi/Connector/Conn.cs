5c409c3f9ec173e0bd9571f148ffc788d9d9f6a7
Armağan Akpınar <ekber985555@gmail.com>
2021-12-26 17:59:08 +0300

    upload files

---
+++ WebApi/Connector/Conn.cs
@@ -0,0 +1,129 @@
+﻿
+using Pomelo.Data.MySql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using WebApi.Model;
+
+namespace WebApi.Connector
+{
+   
+    public class Conn
+    {
+        private string constring;
+        public Conn()
+        {
+            constring = @"server=localhost;userid=root;password=123456;database=deneme";
+        }
+        public List<Task> Task()
+        {
+            List<Task> allTasks = new List<Task>();
+            
+
+            using (MySqlConnection connMySql= new MySqlConnection(constring))
+            {
+               
+                using (MySqlCommand cmd = connMySql.CreateCommand())
+                {
+                    cmd.CommandText = "Select * from task";
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Connection = connMySql;
+                  
+
+
+                    connMySql.Open();
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                      
+                        while (reader.Read())
+                        {
+                            allTasks.Add(new Task { id = reader.GetInt32(reader.GetOrdinal("Id")), task = reader.GetString(reader.GetOrdinal("Task_name"))});
+                        }
+                    }
+                }
+                connMySql.Close();
+            }
+
+
+         
+
+            return allTasks;
+
+        }
+        public List<Task> AddTask(Task task)
+        {
+            List<Task> allTasks = new List<Task>();
+
+
+            using (MySqlConnection connMySql = new MySqlConnection(constring))
+            {
+
+                using (MySqlCommand cmd = connMySql.CreateCommand())
+                {
+                    cmd.CommandText = "insert into task(Task_name) values(@Task_name)";
+                    string tname;
+                    tname = task.task;
+                    cmd.Parameters.AddWithValue("@Task_name",tname);
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Connection = connMySql;
+
+
+
+                    connMySql.Open();
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            allTasks.Add(new Task { id = reader.GetInt32(reader.GetOrdinal("Id")), task = reader.GetString(reader.GetOrdinal("Task_name")) });
+                        }
+                    }
+                }
+                connMySql.Close();
+            }
+
+
+
+
+            return allTasks;
+
+        }
+        public List<Task> SelectedTask(int id)
+        {
+            List<Task> allTasks = new List<Task>();
+
+
+            using (MySqlConnection connMySql = new MySqlConnection(constring))
+            {
+
+                using (MySqlCommand cmd = connMySql.CreateCommand())
+                {
+                    cmd.CommandText = "Select * from task where id="+id+"";
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Connection = connMySql;
+
+
+
+                    connMySql.Open();
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            allTasks.Add(new Task { id = reader.GetInt32(reader.GetOrdinal("Id")), task = reader.GetString(reader.GetOrdinal("Task_name")) });
+                        }
+                    }
+                }
+                connMySql.Close();
+            }
+
+
+
+
+            return allTasks;
+
+        }
+
+    }
+}
---
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApi.Connector;
+using WebApi.Model;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+       
+        
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            Conn mySqlGet = new Conn();
+            JsonResult jsonResult = new JsonResult(mySqlGet.Task());
+
+           
+            return jsonResult;
+        }
+
+        [HttpPost]
+        public JsonResult Post(Task task)
+        {
+            Conn mySqlGet = new Conn();
+            mySqlGet.AddTask(task);
+            JsonResult jsonResult = new JsonResult("added succesfully");
+
+
+            return jsonResult;
+        }
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            Conn mySqlGet = new Conn();
+            JsonResult jsonResult = new JsonResult(mySqlGet.SelectedTask(id));
+
+            return jsonResult ;
+        }
+    }
+}
